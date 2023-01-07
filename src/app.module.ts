30150bdb33faa3cd0184e0a8f5da894cd38828db
VelvetAccardia <arthur.santana@chat2desk.com.br>
2023-01-07 09:56:57 -0300

    init: nestjs app

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { CustomersModule } from './customers/customers.module';
+import { OrdersModule } from './orders/orders.module';
+import { ProductsModule } from './products/products.module';
+
+@Module({
+  imports: [UsersModule, CustomersModule, OrdersModule, ProductsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
