550b5ab9b6cdf4bbc3a740a498d13b40361c720f
VelvetAccardia <arthur.santana@chat2desk.com.br>
2023-01-08 15:52:21 -0300

    setup: api routes and swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('CRUD API documentation')
+    .setDescription('The documentation for the API routes')
+    .setVersion('v1')
+    .addTag('CRUD')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(4000);
 }
 bootstrap();
